e4953fcdf23d4896071dd04a51794dd142f46771
artemon-line <tyran77757@gmail.com>
2019-02-16 18:06:55 +0200

    finish with auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+  // prod env
+  module.exports = require('./prod');
+} else {
+  // dev env
+  module.exports = require('./dev');
+}
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  // set cookie for existing user
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // Deserialice find user from cookie
+  User.findById(id).then(user => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we have user
+          done(null, existingUser);
+        } else {
+          // create ne db user
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
